a7af5c958437466bf4b19d4e79e493fd72929d0b
Devesh Kumar Mishra <94969888+devn5900@users.noreply.github.com>
2023-01-20 01:23:35 +0530

    day-3

---
+++ src/components/pages/AllBooks/BookCard.jsx
@@ -0,0 +1,97 @@
+import { Icon } from "@chakra-ui/icons";
+import {
+  Button,
+  ButtonGroup,
+  Card,
+  CardBody,
+  CardFooter,
+  Divider,
+  Heading,
+  Image,
+  Text,
+  Stack,
+  Center,
+  Badge,
+  Box,
+  Flex,
+  Grid,
+  GridItem,
+} from "@chakra-ui/react";
+import React from "react";
+import { FaStar } from "react-icons/fa";
+const BookCard = ({
+  title,
+  price,
+  image,
+  id,
+  desc,
+  lang,
+  pages,
+  publisher,
+  rating,
+  year,
+}) => {
+  let rate = [];
+  for (let i = 1; i <= rating; i++) {
+    rate.push(FaStar);
+  }
+
+  return (
+    <Card boxShadow="md" _hover={{ boxShadow: "xl" }}>
+      <CardBody>
+        <Image src={image} alt={title} borderRadius="lg" />
+        <Flex
+          justifyContent="space-between
+        "
+        >
+          <Badge fontSize="0.8em" variant="outline" colorScheme="red">
+            {year}
+          </Badge>
+
+          {rate.length > 0 ? (
+            <Badge fontSize="0.8em" colorScheme="white">
+              {rate &&
+                rate?.map((el, i) => {
+                  return <Icon key={i + 1} color="#3E5962" as={el}></Icon>;
+                })}
+            </Badge>
+          ) : (
+            ""
+          )}
+        </Flex>
+        <Stack>
+          <Heading size="sm">
+            {title.split(" ").slice(1, 3).join(" ")}
+            <Badge ml="1" fontSize="0.8em" colorScheme="green">
+              {lang}
+            </Badge>
+          </Heading>
+
+          <Text size="sm">{desc.substr(0, 40)}</Text>
+          <Center>
+            <Badge fontSize="0.8em">{publisher}</Badge>
+          </Center>
+          <Text color="blue.600" fontSize="xl">
+            ${price}
+          </Text>
+        </Stack>
+        <Center pt="0.3rem" pb="0.3rem">
+          <Button
+            size="sm"
+            bg="#3E5962"
+            color="white"
+            _hover={{
+              border: "1px solid #3E5962",
+              bg: "none",
+              color: "#3E5962",
+            }}
+          >
+            Add To Cart
+          </Button>
+        </Center>
+      </CardBody>
+    </Card>
+  );
+};
+
+export default BookCard;
--- src/components/pages/AllBooks/BookHead.jsx
+++ src/components/pages/AllBooks/BookHead.jsx
@@ -1,16 +1,141 @@
 import {
   Box,
-  Center,
   Flex,
   Heading,
+  Input,
   ListItem,
   Select,
   Text,
   UnorderedList,
 } from "@chakra-ui/react";
-import React from "react";
-
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import BookCards from "./BookCards";
+import { useSearchParams } from "react-router-dom";
+import BookSkeleton from "./BookSkeleton";
 const BookHead = ({ head }) => {
+  const [publisher, setPub] = useState([]);
+  const [language, setLang] = useState([]);
+  const [year, setYear] = useState([]);
+  const [showBook, setShowBook] = useState([]);
+  const fetchData = async () => {
+    const res = await axios.get("http://localhost:3000/books");
+    publish(res.data);
+    lang(res.data);
+    getYear(res.data);
+  };
+  const getYear = (data) => {
+    let p = data.reduce((acc, el) => {
+      acc.push(el.year);
+      return acc;
+    }, []);
+    const myset = new Set(p);
+    setYear([...myset]);
+  };
+  const lang = (data) => {
+    let p = data.reduce((acc, el) => {
+      acc.push(el.lang);
+      return acc;
+    }, []);
+    const myset = new Set(p);
+    setLang([...myset]);
+  };
+  const publish = (data) => {
+    let p = data.reduce((acc, el) => {
+      acc.push(el.publisher);
+      return acc;
+    }, []);
+    const myset = new Set(p);
+    setPub([...myset]);
+  };
+  const [searchParam, setSearchParam] = useSearchParams();
+  const [page, setPage] = useState(searchParam.get("_page") || 1);
+  const [search, setSearch] = useState(searchParam.get("q") || undefined);
+  const [load, setLoad] = useState(false);
+  const [sort, setSort] = useState({
+    by: searchParam.get("_sort") || "",
+    order: searchParam.get("_order") || "",
+  });
+  const [filter, setFilter] = useState({
+    publisher: searchParam.get("publisher") || "",
+    year: searchParam.get("year") || "",
+    lang: searchParam.get("lang") || "",
+  });
+  const [pagination, setPagination] = useState(0);
+  useEffect(() => {
+    fetchData();
+    fetchShowBook(page, search, sort, filter);
+  }, [page, search, sort, filter]);
+  const baseUrl = (page, search, sort, filter) => {
+    if (search) {
+      return `http://localhost:3000/books?_page=${page}&_limit=12&q=${search}`;
+    } else if (filter.publisher) {
+      return `http://localhost:3000/books?_page=${page}&_limit=12&publisher=${filter.publisher}`;
+    } else if (filter.year) {
+      return `http://localhost:3000/books?_page=${page}&_limit=12&publisher=${filter.year}`;
+    } else if (filter.lang) {
+      return `http://localhost:3000/books?_page=${page}&_limit=12&publisher=${filter.lang}`;
+    } else if (filter.publisher && filter.year && filter.lang) {
+      return `http://localhost:3000/books?_page=${page}&_limit=12&publisher=${filter.lang}&year=${filter.year}&lang=${filter.lang}`;
+    } else {
+      return `http://localhost:3000/books?_page=${page}&_limit=12&_sort=${sort.by}&_order=${sort.order}`;
+    }
+  };
+  const fetchShowBook = (page, search, sort, filter) => {
+    setLoad(true);
+    axios
+      .get(baseUrl(page, search, sort, filter))
+      .then((res) => {
+        setPagination(res.headers.get("x-total-count"));
+        setShowBook(res.data);
+        setLoad(false);
+      })
+      .catch((err) => {
+        setLoad(false);
+      });
+  };
+
+  const updatePage = (val) => {
+    setPage((prev) => {
+      let inc = Number(prev) + val;
+      setSearchParam({
+        _page: inc,
+      });
+
+      return inc;
+    });
+  };
+
+  const searchBook = (e) => {
+    setSearch(e.target.value);
+    setSearchParam({ _page: page, q: e.target.value });
+  };
+
+  const sortByPrice = (e) => {
+    // setSearchParam({ _sort: "", _order: "" });
+    setSort({ by: e.target.name, order: e.target.value });
+    setSearchParam({
+      _page: page,
+      _sort: e.target.name,
+      _order: e.target.value,
+    });
+  };
+
+  const filterBy = (e) => {
+    setFilter((prev) => {
+      let f = { ...prev, [e.target.name]: e.target.value };
+      let obj = {};
+      for (let key in prev) {
+        if (prev[key]) {
+          obj[key] = prev[key];
+        }
+      }
+      setSearchParam({ _page: page, ...obj, [e.target.name]: e.target.value });
+      return f;
+    });
+  };
+  let obj1 = searchParam.entries();
+  console.log(obj1);
   return (
     <Box mt="1rem">
       <Box>
@@ -23,39 +148,119 @@ const BookHead = ({ head }) => {
           </Text>
         </Flex>
       </Box>
-      <Box w="90%">
-        <Flex>
-          <Box borderRight="1px solid lightgray" p="1rem">
+      <Box>
+        <Flex justifyContent="space-around">
+          <Box
+            borderRight="1px solid lightgray"
+            w="20%"
+            pt="3rem"
+            pl="2rem"
+            pr="2rem"
+          >
             <Box>
-              <UnorderedList listStyleType="none">
+              <UnorderedList listStyleType="none" spacing="0.7rem">
+                <Heading size="md" mb="0.7rem">
+                  Search Book
+                </Heading>
+                <ListItem>
+                  <Box>
+                    <Input
+                      type="search"
+                      htmlSize={100}
+                      onChange={searchBook}
+                      borderRadius="none"
+                      placeholder="Search by Title, Author, Keyword or ISBN"
+                    />
+                  </Box>
+                </ListItem>
+              </UnorderedList>
+              <UnorderedList listStyleType="none" mt="0.7rem" spacing="0.7rem">
                 <Heading size="md" mb="0.7rem">
                   Sort By{" "}
                 </Heading>
                 <ListItem>
-                  <Select placeholder="Price">
-                    <option value="lToh">Low to High</option>
-                    <option value="hTol">High to Low</option>
+                  <Select onChange={sortByPrice} name="price">
+                    <option value="">Price</option>
+                    <option value="asc">Low to High</option>
+                    <option value="desc">High to Low</option>
                   </Select>
                 </ListItem>
                 <ListItem>
                   {" "}
-                  <Select placeholder="Rating">
-                    <option value="lToh">Low to High</option>
-                    <option value="hTol">High to Low</option>
+                  <Select name="rating" onChange={sortByPrice}>
+                    <option value="">Rating</option>
+                    <option value="asc">Low to High</option>
+                    <option value="desc">High to Low</option>
+                  </Select>
+                </ListItem>
+              </UnorderedList>
+              <UnorderedList listStyleType="none" mt="0.7rem" spacing="0.7rem">
+                <Heading size="md" mb="0.7rem">
+                  Fliter By
+                </Heading>
+                <ListItem>
+                  <Select
+                    placeholder="Publisher"
+                    onChange={filterBy}
+                    name="publisher"
+                  >
+                    {publisher &&
+                      publisher?.map((el, i) => {
+                        return (
+                          <option key={i + 1} value={el}>
+                            {el}
+                          </option>
+                        );
+                      })}
                   </Select>
                 </ListItem>
                 <ListItem>
                   {" "}
-                  <Select placeholder="Page">
-                    <option value="lToh">Low to High</option>
-                    <option value="hTol">High to Low</option>
+                  <Select
+                    placeholder="Language"
+                    onChange={filterBy}
+                    name="lang"
+                  >
+                    {language &&
+                      language?.map((el, i) => {
+                        return (
+                          <option key={i + 1} value={el}>
+                            {el}
+                          </option>
+                        );
+                      })}
                   </Select>
                 </ListItem>
-                <ListItem>Facilisis in pretium nisl aliquet</ListItem>
+                <ListItem>
+                  {" "}
+                  <Select placeholder="Year" onChange={filterBy} name="year">
+                    {year &&
+                      year?.map((el, i) => {
+                        return (
+                          <option key={i + 1} value={el}>
+                            {el}
+                          </option>
+                        );
+                      })}
+                  </Select>
+                </ListItem>
               </UnorderedList>
             </Box>
           </Box>
-          <Box>right</Box>
+          <Box>
+            <Flex alignItems="center">
+              {!load ? (
+                <BookCards
+                  data={showBook}
+                  pagination={pagination}
+                  updatePage={updatePage}
+                  page={page}
+                />
+              ) : (
+                <BookSkeleton />
+              )}
+            </Flex>
+          </Box>
         </Flex>
       </Box>
     </Box>
